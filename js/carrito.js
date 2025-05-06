d40a0079193c90a0b399ac63fac53137e45fc8dd
Fede Cuello <fedecuello95@gmail.com>
2025-05-06 20:56:41 -0300

    agregar botones en el carrito, alertas, correccion de rutas, numero dinamico en carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,18 +1,26 @@
 let carroIphones = JSON.parse(localStorage.getItem("carroIphones")) || [];
-
+let iphones = JSON.parse(localStorage.getItem("iphones")) || [];
 let contenedorCarro = document.getElementById("contenedor-carro");
 
 function mostrarCarro(iphonesCarro) {
+  contenedorCarro.innerHTML = ""
   if (iphonesCarro.length === 0) {
     const mensajeVacio = document.createElement("h3");
-    mensajeVacio.textContent = "Tu carrito esta vacio";
-    contenedorCarro.appendChild(mensajeVacio);
-    return;
+    mensajeVacio.textContent = "Tu carrito esta vacio"
+    contenedorCarro.appendChild(mensajeVacio)
+    return
   }
 
   iphonesCarro.forEach((iphone) => {
+
+    let botonEstado = ""
+
+    if (iphone.stock <= 0) {
+      botonEstado = "disabled"
+    }
+
     const cardCarro = document.createElement("div");
-    cardCarro.classList.add("col-md-2", "mb-4");
+    cardCarro.classList.add("col-md-2", "mb-4")
     cardCarro.innerHTML = `
                             <div class="card">
                             <img src="${iphone.imagen}" class="card-img-top" alt="${iphone.modelo}">
@@ -23,34 +31,186 @@ function mostrarCarro(iphonesCarro) {
                             <p class="card-carro-text">Color: ${iphone.color}</p>
                             <p class="card-carro-text">Estado: ${iphone.estado}</p>
                             <p class="card-carro-text">Batería: ${iphone.bateria}%</p>
+                            <p class="card-carro-text">Subtotal: ${iphone.cantidad * iphone.precio} USD </p>
+                            <button class="restar-cantidad" id="${iphone.id}">-</button>
                             <p class="card-carro-text">Cantidad: ${iphone.cantidad}</p>
+                            <button class="sumar-cantidad" id="${iphone.id}" ${botonEstado}>+</button>
+                            <button class="eliminar-producto" id="${iphone.id}">Eliminar Producto</button>
                             </div>
                             </div>`;
-    contenedorCarro.appendChild(cardCarro);
-  });
+    contenedorCarro.appendChild(cardCarro)
+  })
+
+  const contenedorFinal = document.createElement("div");
+  contenedorFinal.classList.add ("contenedor-final")
 
   const botonVaciar = document.createElement("button");
-  botonVaciar.textContent = "Vaciar carrito";
-  botonVaciar.classList.add("vaciar-carrito");
-  botonVaciar.onclick = vaciarCarrito;
-  contenedorCarro.appendChild(botonVaciar);
-
-  const sumaPrecios = iphonesCarro.reduce(
-    (contador, iphone) => contador + iphone.precio * iphone.cantidad,
-    0
-  );
+  botonVaciar.textContent = "Vaciar carrito"
+  botonVaciar.classList.add("vaciar-carrito")
+  botonVaciar.onclick = vaciarCarrito
+  
+  const sumaPrecios = iphonesCarro.reduce((contador, iphone) => contador + iphone.precio * iphone.cantidad,0);
 
   const precioTotal = document.createElement("h3");
-  precioTotal.textContent = `Total de la compra: ${sumaPrecios} USD`;
+  precioTotal.textContent = `Total de la compra: ${sumaPrecios} USD`
 
-  contenedorCarro.appendChild(precioTotal);
+  const botonFinalizar = document.createElement("button");
+  botonFinalizar.textContent = "Finalizar Compra"
+  botonFinalizar.classList.add("boton-finalizar")
+  botonFinalizar.id = "finalizarCompra"
+
+  contenedorFinal.appendChild(precioTotal)
+  contenedorFinal.appendChild(botonVaciar)
+  contenedorFinal.appendChild(botonFinalizar)
+
+
+  contenedorCarro.appendChild(contenedorFinal)
+
+  document.getElementById("finalizarCompra").addEventListener("click", () => {
+    const swalWithBootstrapButtons = Swal.mixin({
+      customClass: {
+        confirmButton: "btn btn-success",
+        cancelButton: "btn btn-danger",
+      },
+      buttonsStyling: false,
+    });
+    swalWithBootstrapButtons
+      .fire({
+        title: "Confirmar compra",
+        icon: "info",
+        showCancelButton: true,
+        confirmButtonText: "Si,terminar compra",
+        cancelButtonText: "No,seguir buscando",
+        reverseButtons: true,
+      })
+      .then((result) => {
+        if (result.isConfirmed) {
+          swalWithBootstrapButtons.fire({
+            title: "Listo",
+            text: "Compra exitosa",
+            icon: "success",
+          });
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          swalWithBootstrapButtons.fire({
+            title: "No finalizaste tu compra",
+            text: "Vuelve a ver mas productos",
+            icon: "info",
+          });
+        }
+      });
+  });
+
+  agregarEventosBotonesCarrito(); 
 }
 
 function vaciarCarrito() {
-  localStorage.removeItem("carroIphones");
-  carroIphones = [];
-  contenedorCarro.innerHTML = "";
-  mostrarCarro(carroIphones);
+  Swal.fire({
+    title: "¿Estás seguro?",
+    text: "Esta acción vaciará todo el carrito.",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Sí, vaciar",
+    cancelButtonText: "Cancelar",
+    confirmButtonColor: "#d33",
+    cancelButtonColor: "#3085d6",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      carroIphones.forEach((iphonesEnCarro) => {const stockInicial = iphones.find((iphone) => iphone.id === iphonesEnCarro.id)
+        if (stockInicial) {
+          stockInicial.stock += iphonesEnCarro.cantidad
+        }
+      })
+
+      localStorage.removeItem("carroIphones")
+      carroIphones = []
+      contenedorCarro.innerHTML = ""
+
+     localStorage.setItem("iphones", JSON.stringify(iphones))
+
+      mostrarCarro(carroIphones)
+
+      Swal.fire({
+        icon: "success",
+        title: "Carrito vaciado",
+        text: "Todos los productos fueron eliminados.",
+        confirmButtonColor: "#178582",
+      })
+    }
+  })
 }
 
-mostrarCarro(carroIphones);
+mostrarCarro(carroIphones)
+
+//contenedorCarro.addEventListener("click", botonesCarrito);
+
+//function botonesCarrito(e) {
+ // const id = parseInt(e.target.id);
+//}
+
+
+function agregarEventosBotonesCarrito() {
+ const botonesSumar = document.querySelectorAll(".sumar-cantidad");
+  botonesSumar.forEach((boton) => {boton.addEventListener("click", (e) => {
+      const id = parseInt(e.target.id);
+      const productoElegido = carroIphones.find((iphone) => iphone.id === id);
+      const stockOriginal = iphones.find((iphone) => iphone.id === id);
+
+      if (productoElegido && stockOriginal && stockOriginal.stock > 0) {
+        productoElegido.cantidad++
+        stockOriginal.stock--
+
+        localStorage.setItem("carroIphones", JSON.stringify(carroIphones))
+        localStorage.setItem("iphones", JSON.stringify(iphones))
+        mostrarCarro(carroIphones)
+        
+      }
+    })
+  })
+
+  const botonesRestar = document.querySelectorAll(".restar-cantidad");
+  botonesRestar.forEach((boton) => {boton.addEventListener("click", (e) => {
+      const id = parseInt(e.target.id);
+      const productoElegido = carroIphones.find((iphone) => iphone.id === id);
+      const stockOriginal = iphones.find((iphone) => iphone.id === id);
+
+      //console.log("producto antes de restar:", productoElegido);
+
+      if (productoElegido && stockOriginal) {
+        if (productoElegido.cantidad > 1) {
+          //console.log("Restando 1 a la cantidad");
+          productoElegido.cantidad--
+          stockOriginal.stock++
+        } else {
+          //console.log("eliminando producto del carrito");
+          carroIphones = carroIphones.filter((iphone) => iphone.id !== id)
+          stockOriginal.stock++ 
+        }
+
+        //console.log("despues de restar:", productoElegido);
+        localStorage.setItem("carroIphones", JSON.stringify(carroIphones))
+        localStorage.setItem("iphones", JSON.stringify(iphones))
+
+       mostrarCarro(carroIphones)
+        
+      }
+    })
+  })
+
+  const botonesEliminar = document.querySelectorAll(".eliminar-producto");
+  botonesEliminar.forEach((boton) => {boton.addEventListener("click", (e) => {
+      const id = parseInt(e.target.id);
+      const productoElegido = carroIphones.find((iphone) => iphone.id === id);
+      const stockOriginal = iphones.find((iphone) => iphone.id === id);
+
+      if (productoElegido && stockOriginal) {
+        stockOriginal.stock += productoElegido.cantidad
+        carroIphones = carroIphones.filter((iphone) => iphone.id !== id)
+
+        localStorage.setItem("carroIphones", JSON.stringify(carroIphones))
+        localStorage.setItem("iphones", JSON.stringify(iphones))
+        mostrarCarro(carroIphones)
+        
+      }
+    })
+  })
+}
--- js/index.js
+++ js/index.js
@@ -1,104 +1,110 @@
-const iphones = [
-  {
-    modelo: "iPhone 12",
-    precio: 700,
-    almacenamiento: "128GB",
-    color: "Negro",
-    estado: "Usado",
-    bateria: 80,
-    imagen: "/assets/iphone12negro.webp",
-    id: 1,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 12 PRO",
-    precio: 800,
-    almacenamiento: "258GB",
-    color: "Azul",
-    estado: "Usado",
-    bateria: 80,
-    imagen: "/assets/iphone12proazul.webp",
-    id: 2,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 13",
-    precio: 850,
-    almacenamiento: "128GB",
-    color: "Blanco",
-    estado: "Nuevo",
-    bateria: 100,
-    imagen: "/assets/iphone13blanco.webp",
-    id: 3,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 13 PRO",
-    precio: 850,
-    almacenamiento: "128GB",
-    color: "Gris",
-    estado: "Usado",
-    bateria: 95,
-    imagen: "/assets/iphone13progris.png",
-    id: 4,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 14",
-    precio: 950,
-    almacenamiento: "256GB",
-    color: "Rojo",
-    estado: "Nuevo",
-    bateria: 100,
-    imagen: "/assets/iphone14rojo.png",
-    id: 5,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 14 PRO",
-    precio: 1050,
-    almacenamiento: "512GB",
-    color: "Negro",
-    estado: "Nuevo",
-    bateria: 100,
-    imagen: "/assets/iphone14pronegro.webp",
-    id: 6,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 14 PRO",
-    precio: 900,
-    almacenamiento: "512GB",
-    color: "Morado",
-    estado: "Usado",
-    bateria: 85,
-    imagen: "/assets/iphone14promorado.jpg",
-    id: 7,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 15",
-    precio: 1000,
-    almacenamiento: "512GB",
-    color: "Rosa",
-    estado: "Usado",
-    bateria: 90,
-    imagen: "/assets/iphone15rosa.webp",
-    id: 8,
-    stock: 2,
-  },
-  {
-    modelo: "iPhone 15 PRO",
-    precio: 1200,
-    almacenamiento: "512GB",
-    color: "Titanium",
-    estado: "Nuevo",
-    bateria: 100,
-    imagen: "/assets/iphone15protitanio.webp",
-    id: 9,
-    stock: 2,
-  },
-];
+let iphones = JSON.parse(localStorage.getItem("iphones"));
+
+if (!iphones) {
+  iphones = [
+    {
+      modelo: "iPhone 12",
+      precio: 700,
+      almacenamiento: "128GB",
+      color: "Negro",
+      estado: "Usado",
+      bateria: 80,
+      imagen: "../assets/iphone12negro.webp",
+      id: 1,
+      stock: 5,
+    },
+    {
+      modelo: "iPhone 12 PRO",
+      precio: 800,
+      almacenamiento: "258GB",
+      color: "Azul",
+      estado: "Usado",
+      bateria: 80,
+      imagen: "../assets/iphone12proazul.webp",
+      id: 2,
+      stock: 6,
+    },
+    {
+      modelo: "iPhone 13",
+      precio: 850,
+      almacenamiento: "128GB",
+      color: "Blanco",
+      estado: "Nuevo",
+      bateria: 100,
+      imagen: "../assets/iphone13blanco.webp",
+      id: 3,
+      stock: 3,
+    },
+    {
+      modelo: "iPhone 13 PRO",
+      precio: 850,
+      almacenamiento: "128GB",
+      color: "Gris",
+      estado: "Usado",
+      bateria: 95,
+      imagen: "../assets/iphone13progris.png",
+      id: 4,
+      stock: 4,
+    },
+    {
+      modelo: "iPhone 14",
+      precio: 950,
+      almacenamiento: "256GB",
+      color: "Rojo",
+      estado: "Nuevo",
+      bateria: 100,
+      imagen: "../assets/iphone14rojo.png",
+      id: 5,
+      stock: 2,
+    },
+    {
+      modelo: "iPhone 14 PRO",
+      precio: 1050,
+      almacenamiento: "512GB",
+      color: "Negro",
+      estado: "Nuevo",
+      bateria: 100,
+      imagen: "../assets/iphone14pronegro.webp",
+      id: 6,
+      stock: 3,
+    },
+    {
+      modelo: "iPhone 14 PRO",
+      precio: 900,
+      almacenamiento: "512GB",
+      color: "Morado",
+      estado: "Usado",
+      bateria: 85,
+      imagen: "../assets/iphone14promorado.jpg",
+      id: 7,
+      stock: 5,
+    },
+    {
+      modelo: "iPhone 15",
+      precio: 1000,
+      almacenamiento: "512GB",
+      color: "Rosa",
+      estado: "Usado",
+      bateria: 90,
+      imagen: "../assets/iphone15rosa.webp",
+      id: 8,
+      stock: 4,
+    },
+    {
+      modelo: "iPhone 15 PRO",
+      precio: 1200,
+      almacenamiento: "512GB",
+      color: "Titanium",
+      estado: "Nuevo",
+      bateria: 100,
+      imagen: "../assets/iphone15protitanio.webp",
+      id: 9,
+      stock: 2,
+    },
+  ]
+
+  localStorage.setItem("iphones", JSON.stringify(iphones))
+}
 
 let carroIphones = JSON.parse(localStorage.getItem("carroIphones")) || [];
 
@@ -108,8 +114,18 @@ function iphonesCards(iphones) {
   const contenedor = document.getElementById("contenedor-productos");
 
   iphones.forEach((iphone) => {
-    const card = document.createElement("div")
-    card.classList.add("col-md-3", "mb-4")
+
+    
+    let botonTexto = "RESERVAR"
+    let botonEstado = ""
+
+    if (iphone.stock <= 0) {
+      botonTexto = "SIN STOCK"
+      botonEstado = "disabled"
+    }
+
+    const card = document.createElement("div");
+    card.classList.add("col-md-3", "mb-4");
     card.innerHTML = `
       <div class="card">
         <img src="${iphone.imagen}" class="card-img-top" alt="${iphone.modelo}">
@@ -120,52 +136,96 @@ function iphonesCards(iphones) {
           <p class="card-text">Color: ${iphone.color}</p>
           <p class="card-text">Estado: ${iphone.estado}</p>
           <p class="card-text">Batería: ${iphone.bateria}%</p>
-          <button class="reservarIphone" id="${iphone.id}">RESERVAR</button>
+          <button class="reservarIphone" id="${iphone.id}" ${botonEstado}>${botonTexto}</button>
         </div>
       </div>
-    `;
+    `
 
     contenedor.appendChild(card)
-  });
+  })
   reservarIphone();
 }
 
 iphonesCards(iphones);
+contadorCarro();
 
 //reservar con boton
 
 function reservarIphone() {
-  btnComprar = document.querySelectorAll(".reservarIphone")
-  btnComprar.forEach((button) => {
-    button.onclick = (e) => {
-      const iphoneId = e.currentTarget.id
-      const iphoneElegido = iphones.find((iphone) => iphone.id == iphoneId)
-      const iphoneExistente = carroIphones.find((iphone) => iphone.id === iphoneElegido.id)
-
-      if (iphoneExistente) {
-        iphoneExistente.cantidad += 1;
-      } else {
-        iphoneElegido.cantidad = 1;
-        carroIphones.push(iphoneElegido);
+  const btnComprar = document.querySelectorAll(".reservarIphone");
+  btnComprar.forEach((button) => {button.onclick = (e) => {
+      const iphoneId = parseInt(e.currentTarget.id); 
+      const iphoneElegido = iphones.find((iphone) => iphone.id === iphoneId);
+      const boton = e.currentTarget;
+     
+      if (iphoneElegido.stock === 0) {
+       boton.textContent = "AGOTADO"
+       boton.disabled = true
+       return
       }
-      localStorage.setItem("carroIphones", JSON.stringify(carroIphones));
-    };
-  });
+      
+      const iphoneEnCarro = carroIphones.find((iphone) => iphone.id === iphoneId);
+
+      if (iphoneEnCarro) {
+        iphoneEnCarro.cantidad++
+        iphoneElegido.stock--
+      
+        
+      } else {
+        const nuevoIphone = {
+          id: iphoneElegido.id,
+          modelo: iphoneElegido.modelo,
+          precio: iphoneElegido.precio,
+          imagen: iphoneElegido.imagen,
+          color: iphoneElegido.color,
+          almacenamiento: iphoneElegido.almacenamiento,
+          estado: iphoneElegido.estado,
+          bateria: iphoneElegido.bateria,
+          cantidad: 1,
+          
+        }
+
+        carroIphones.push(nuevoIphone)
+        iphoneElegido.stock--
+        
+      }
+
+      
+      if (iphoneElegido.stock === 0) {
+        boton.textContent = "SIN STOCK"
+        boton.disabled = true
+      }
+      
+      localStorage.setItem("iphones", JSON.stringify(iphones))
+      localStorage.setItem("carroIphones", JSON.stringify(carroIphones))
+      contadorCarro()
+    }
+  })
 }
 
+
+
+
+function contadorCarro() {
+  const numeroCarro = document.getElementById("numeroCarro");
+  let cantidadCarro = carroIphones.reduce((contador, iphone) => contador + iphone.cantidad, 0)
+  numeroCarro.innerText = cantidadCarro
+}
+
+
 //buscador
-const inputBuscador = document.getElementById("buscador")
-const btnBuscar = document.getElementById("btn-buscar")
+const inputBuscador = document.getElementById("buscador");
+const btnBuscar = document.getElementById("btn-buscar");
 
 function buscarIphones() {
-  const texto = inputBuscador.value.toLowerCase()
-  const buscados = iphones.filter((iphone) =>
-      iphone.modelo.toLowerCase().includes(texto) || iphone.estado.toLowerCase().includes(texto))
-  const contenedor = document.getElementById("contenedor-productos")
+  const texto = inputBuscador.value.toLowerCase();
+  const buscados = iphones.filter((iphone) =>iphone.modelo.toLowerCase().includes(texto) ||iphone.estado.toLowerCase().includes(texto));
+  const contenedor = document.getElementById("contenedor-productos");
   contenedor.innerHTML = ""
-  
-  iphonesCards(buscados);
+
+  iphonesCards(buscados)
 }
 
 inputBuscador.addEventListener("input", buscarIphones)
 btnBuscar.addEventListener("click", buscarIphones)
+
